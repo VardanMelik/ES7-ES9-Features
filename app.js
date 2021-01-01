e66da51ead83323e5bcaeb4129fdd47efa8f50c9
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-01 23:14:08 +0400

    update

--- app.js
+++ app.js
@@ -40,6 +40,38 @@ const userVal = {
 const es9Val = Object.values(userVal);
 
 
+// We use for cloning Objects
+//getOwnPropertyDescriptors()
+
+// Setter  Getter
+const person = {
+    name: 'Max',
+    age: 30,
+    set personName(name) {
+        this.name = name;
+    },
+    get password() {
+        return `${this.name} ${this.age}`;
+    },
+};
+
+const object1 = {
+    property1: 42
+};
+
+const descriptor1 = Object.getOwnPropertyDescriptor(object1, 'property1');
+
+console.log(descriptor1.configurable);
+
+console.log(descriptor1.value);
+
+
+/*console.log(person);
+console.log(person.password);
+console.log(Object.getOwnPropertyDescriptor(person));
+*/
+
+
 app.get('/es9', (req, res) => {
     res.json(es9Val);
 })
